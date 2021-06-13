084a240f9cfbaab4ae9ba7d8fa0edf9dd8befeed
mgmoldova <mgmoldova@ya.ru>
2021-06-13 21:49:43 +0300

    live server

--- camera.py
+++ camera.py
@@ -6,7 +6,9 @@ class Camera(object):
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
     def __init__(self):
-        self.frames = open("1" + '.jpg', 'rb').read() for f in ['1']
+    	f = open("1" + '.jpg', 'rb')
+        self.frames = f.read()
+        f.close()
 
     def get_frame(self):
         return self.frames[0]
